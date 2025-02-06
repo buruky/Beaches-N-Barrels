3db0f685a1b0852de540fdedbc39688c22118893
buruk yimesgen <buruky@uw.edu>
2025-02-05 16:10:31 -0800

    set up psuedocode for run class

--- Run.py
+++ Run.py
@@ -1,1 +1,29 @@
 
+import pygame
+
+pygame.init()
+class Run:
+    def ControllerTick():
+        #Handle Input Events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return 0
+            elif event.type == MOUSEBUTTONDOWN:
+                fist.punch()
+            elif event.type is MOUSEBUTTONUP:
+                fist.unpunch()
+        return 1
+
+    def ViewTick():
+        #Draw Everything
+        ...
+
+    def main():
+        ...
+        while 1:
+
+            if ControllerTick() == 0:
+                return
+
+            ViewTick()
+
