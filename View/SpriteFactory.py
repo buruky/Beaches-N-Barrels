17d4e95fb62e7a005b227b729bd11f6f69fc2043
buruk yimesgen <buruky@uw.edu>
2025-03-15 12:31:51 -0700

    manually merged Sprite Factory

--- View/SpriteFactory.py
+++ View/SpriteFactory.py
@@ -3,6 +3,8 @@ import os
 import pygame
 from .SpriteSheet import SpriteSheet
 from ViewUnits import ViewUnits
+import re
+from typing import Final
 
 class SpriteFactory:
     
@@ -10,22 +12,20 @@ class SpriteFactory:
         """Loads all sprites and prepares them for use."""
         current_directory = os.path.dirname(__file__)
         assets_path = os.path.join(current_directory, '..', 'Assets')
+        self.DOLPHIN_STRING:Final = "Dolphin"
+        self.BUDDHA_STRING:Final = "Buddha"
+        self.ASTRONAUT_STRING:Final = "Astronaut"
+
+        self.current_dir = os.path.dirname(os.path.dirname(__file__))  # Get the directory where the current script is located
+        assets_path = os.path.join(self.current_dir, "Assets")
 
         # Load Player Images
-        self.myRawPlayerImage = pygame.image.load(os.path.join(assets_path, 'luffy.png'))
-        self.myRawDolphinImage = pygame.image.load(os.path.join(assets_path, 'Dolphin.png'))  
-        self.myRawBuddhaImage = pygame.image.load(os.path.join(assets_path, 'Buddha.jpg'))  # NEW
-        self.myRawAstronautImage = pygame.image.load(os.path.join(assets_path, 'Astronaut.jpg'))  # NEW
-
-        # Load Enemy Images
         self.myRawEnemyImage = pygame.image.load(os.path.join(assets_path, 'speederman.png'))
         self.myRawPirateImage = pygame.image.load(os.path.join(assets_path, 'pirate.png'))
         self.myRawCrabImage = pygame.image.load(os.path.join(assets_path, 'crab.png'))
         self.myRawBeachBallImage = pygame.image.load(os.path.join(assets_path, 'BeachBall.png'))
         self.myRawSeagullImage = pygame.image.load(os.path.join(assets_path, 'seagull.png'))
         self.myRawSharkImage = pygame.image.load(os.path.join(assets_path, 'Shark.png'))
-
-        # Load Projectile Image
         self.myRawProjectileImage = pygame.image.load(os.path.join(assets_path, 'projectileDolphin.png'))
         self.myRaw2ProjectileImage = pygame.image.load(os.path.join(assets_path, 'projectileBuddha.png'))
         self.myRaw3ProjectileImage = pygame.image.load(os.path.join(assets_path, 'projectileAstronaut.png'))
@@ -33,8 +33,6 @@ class SpriteFactory:
 
 
         # Resize and Flip Sprites
-        self.myPlayerImage = pygame.transform.scale(self.myRawPlayerImage, ViewUnits.DEFAULT_SPRITE_DIM)
-        self.myPlayerImage2 = pygame.transform.flip(self.myPlayerImage, True, False)
 
         self.dolphinImage = pygame.transform.scale(self.myRawDolphinImage, ViewUnits.DEFAULT_SPRITE_DIM)  
         self.dolphinImage2 = pygame.transform.flip(self.dolphinImage, True, False)  
@@ -102,27 +100,47 @@ class SpriteFactory:
             return self.createProjectileBuddhaSpriteSheet(theId, thePositionX, thePositionY)
         elif theName == "ProjectileAstronaut" or theName == "ProjectilePirate":
             return self.createProjectileAstronautSpriteSheet(theId, thePositionX, thePositionY)
+        elif theName == "Door":
+            return self.createDoorSpriteSheet(theId, thePositionX, thePositionY)
         else:  # Default to generic enemy
             return self.createEnemySpriteSheet(theId, thePositionX, thePositionY)
 
+    def getAssetList(self,theName:str, theState:str):
 
-    def createPlayerSpriteSheet(self, theId, thePositionX, thePositionY):
-        """Creates a player sprite sheet."""
-        imageDict = {ViewUnits.DEFAULT_STATE_NAME: [self.myPlayerImage, self.myPlayerImage2]}
-        playerRect = copy.copy(ViewUnits.DEFAULT_RECT)
-        playerRect.x = thePositionX
-        playerRect.y = thePositionY
+        assets_folder = os.path.join(self.current_dir,"Assets/" + theName)  # Assuming 'assets' folder is in the same directory as the script
 
-        return SpriteSheet(theId, ViewUnits.PLAYER_SPRITE_NAME, imageDict, playerRect)
+        images = []
+        image_files = [f for f in os.listdir(assets_folder) if f.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif'))]
 
+        for image_file in image_files:
+            if re.search(theState, image_file):
+                image_path = os.path.join(assets_folder, image_file)
+                image = pygame.image.load(image_path)
+                images.append(image)
+        return images
     def createDolphinSpriteSheet(self, theId, thePositionX, thePositionY):
         """Creates a Dolphin sprite sheet."""
         imageDict = {ViewUnits.DEFAULT_STATE_NAME: [self.dolphinImage, self.dolphinImage2]}  
+        imageDict = {ViewUnits.DEFAULT_STATE_NAME: self.getAssetList(self.DOLPHIN_STRING, ViewUnits.DEFAULT_STATE_NAME),
+                     ViewUnits.DIRECTION_UP: self.getAssetList(self.DOLPHIN_STRING, ViewUnits.DIRECTION_UP),
+                     ViewUnits.DIRECTION_DOWN: self.getAssetList(self.DOLPHIN_STRING,ViewUnits.DIRECTION_DOWN),
+                     ViewUnits.DIRECTION_LEFT: self.getAssetList(self.DOLPHIN_STRING, ViewUnits.DIRECTION_LEFT),
+                     ViewUnits.DIRECTION_RIGHT: self.getAssetList(self.DOLPHIN_STRING, ViewUnits.DIRECTION_RIGHT),
+                     }  
         dolphinRect = copy.copy(ViewUnits.DEFAULT_RECT)
         dolphinRect.x = thePositionX
         dolphinRect.y = thePositionY
+        return SpriteSheet(theId, self.DOLPHIN_STRING, imageDict, dolphinRect)
 
-        return SpriteSheet(theId, "Dolphin", imageDict, dolphinRect)  
+    def createDoorSpriteSheet(self, theId, thePositionX, thePositionY):
+        imageDict = {
+                     ViewUnits.DIRECTION_UP: self.getAssetList("Door", ViewUnits.DIRECTION_UP),
+                     ViewUnits.DIRECTION_DOWN: self.getAssetList("Door",ViewUnits.DIRECTION_DOWN),
+                     ViewUnits.DIRECTION_LEFT: self.getAssetList("Door", ViewUnits.DIRECTION_LEFT),
+                     ViewUnits.DIRECTION_RIGHT: self.getAssetList("Door", ViewUnits.DIRECTION_RIGHT),
+                     }  
+        DoorRect = pygame.Rect((thePositionX,thePositionY), (64,64))
+        return SpriteSheet(theId, "Door", imageDict, DoorRect)  
     
     def createSharkSpriteSheet(self, theId, thePositionX, thePositionY):
         """Creates a Dolphin sprite sheet."""
