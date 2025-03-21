08a1666eaf11cfbcaa655ae244c77b336c0eb102
pquaidoo <pquaid@uw.edu>
2025-03-20 17:08:48 -0700

    added  player import

--- Model/__init__.py
+++ Model/__init__.py
@@ -5,10 +5,11 @@ from .EventManager import EventManager
 from .Floor import Floor
 from .GameWorld import GameWorld
 from .Room import Room
-
+from .Player import Player
 
 # Define what is available when importing `model`
-__all__ = ["DungeonCharacter", "DungeonCharacterList", "EventManager","Floor","GameWorld","Room"]
+__all__ = ["DungeonCharacter", "DungeonCharacterList", "EventManager","Floor","GameWorld","Room",
+            "Player"]
 
 
 
